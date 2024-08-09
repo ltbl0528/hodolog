af10f6015cd7ded781b9acf468788a7483dee92c
ltbl0528 <ltbl0528@gmail.com>
2024-08-09 17:40:39 +0900

    [EDIT] 호돌로그 섹션 1. 작성글 저장 1 - 게시글 저장 구현(中)

--- src/main/java/com/hodolog/api/controller/PostController.java
+++ src/main/java/com/hodolog/api/controller/PostController.java
@@ -1,7 +1,9 @@
 package com.hodolog.api.controller;
 
 import com.hodolog.api.request.PostCreate;
+import com.hodolog.api.service.PostService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,10 +12,21 @@ import java.util.Map;
 // 데이터 기반 API 응답을 생성
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 public class PostController {
 
+    /**
+     * Controller
+     * ↓ 호출
+     * Service
+     * ↓ 호출
+     * Repository
+     * */
+    private final PostService postService;
+
     @PostMapping("/posts")
-    public Map<String, String> post(@RequestBody @Valid PostCreate params) {
+    public Map<String, String> post(@RequestBody @Valid PostCreate request) {
+        postService.write(request);
         return Map.of();
     }
 }
--- src/test/java/com/hodolog/api/controller/PostControllerTest.java
+++ src/test/java/com/hodolog/api/controller/PostControllerTest.java
@@ -1,26 +1,28 @@
 package com.hodolog.api.controller;
 
+import com.hodolog.api.repository.PostRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private PostRepository postRepository;
 
     @Test
     @DisplayName("/posts 요청 시 hello world를 출력한다.")
@@ -50,4 +52,19 @@ class PostControllerTest {
                 .andDo(print());
     }
 
+
+    @Test
+    @DisplayName("/posts 요청 시 DB에 값이 저장된다.")
+    void test3() throws Exception {
+        // when
+        mockMvc.perform(post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\": \"제목입니다\", \"content\":\"내용입니다.\"}")
+                )
+                .andExpect(status().isOk())
+                .andDo(print());
+
+        // then
+        assertEquals(2L, postRepository.count());
+    }
 }
